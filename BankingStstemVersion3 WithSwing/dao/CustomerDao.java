181e2610a2d093ad508101b0e34773d7ceaa8d58
Adeline Tuyizere <adelinetuyizere333@gmail.com>
2025-05-22 22:02:29 +0200

    customer dao updatw  customer

--- BankingStstemVersion3 WithSwing/dao/CustomerDao.java
+++ BankingStstemVersion3 WithSwing/dao/CustomerDao.java
@@ -51,4 +51,30 @@ public class CustomerDao {
 
    
 
+    // update customer 
+    public int updateCustomer(Customer custObj) {
+        try {
+            Connection con = DriverManager.getConnection(jdbcurl, dbUsername, dbPasswd);
+            String sql = "UPDATE customers SET names = ?, age = ?, phone_number = ?, account_number = ? WHERE nid = ?";
+            PreparedStatement pst = con.prepareStatement(sql);
+
+            pst.setString(1, custObj.getNames());
+            pst.setInt(2, custObj.getAge());
+            pst.setString(3, custObj.getPhone_number());
+            pst.setString(4, custObj.getAccount_number());
+            pst.setString(5, custObj.getNid()); // Condition to update based on NID
+
+            int rowsAffected = pst.executeUpdate();
+
+            pst.close();
+            con.close();
+
+            return rowsAffected;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return 0;
+    }
+
+
 }
