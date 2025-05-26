80278d1b799459436338993698578b02a4ce21f2
Adeline Tuyizere <adelinetuyizere333@gmail.com>
2025-05-26 23:33:22 +0200

    transaction model class for banking system

---
+++ BankingStstemVersion3 WithSwing/model/Transaction.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author adeli
+ */
+public class Transaction {
+    private int id;
+    private Date transactionDate;
+    private Double amount;
+    private ETransactionType type;
+}
+
